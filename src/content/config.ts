cef9ddc2b489b961ee8159dec0430d9c10eebc60
Andy Peacock <1629592+ap1969@users.noreply.github.com>
2025-11-20 23:53:49 +0000

    Make fields optional in posts collection schema
    Updated schema to make fields optional for posts collection.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    excerpt: z.string(),
-    date: z.string(),
-    author: z.string(),
-    authorRole: z.string(),
-    authorAvatar: z.string(),
-    category: z.string(),
-    image: z.string(),
-  }),
-});
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      excerpt: z.string().optional(),
+      date: z.string().optional(),
+      author: z.string().optional(),
+      authorRole: z.string().optional(),
+      authorAvatar: z.string().optional(),
+      category: z.string().optional(),
+      image: z.string().optional(),
+    }),
+  });
 
 export const collections = { posts };
